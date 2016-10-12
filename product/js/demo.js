05f92b02766f803321e85d6a1ba3992f125b5da4
scriptchao <1154806236@qq.com>
2016-10-12 17:34:42 +0800

    changeagain

--- product/js/demo.js
+++ product/js/demo.js
@@ -1,1 +1,14 @@
-function resize(){document.documentElement.style.fontSize=innerWidth/20+"px"}window.onresize=function(){resize()},resize();var i=0,arrDiv=document.querySelectorAll(".fot");for(arrLen=arrDiv.length;i<arrLen;)arrDiv[i].addEventListener("touchstart",function(){console.log("touchstart")}),arrDiv[i].addEventListener("touchmove",function(){console.log("touchmove")}),arrDiv[i].addEventListener("touchend",function(){console.log("touchend")}),i++,console.log("hello me");
+function resize() {
+    document.documentElement.style.fontSize = innerWidth / 20 + "px"
+}
+window.onresize = function () {
+    resize()
+}, resize();
+var i = 0, arrDiv = document.querySelectorAll(".fot");
+for (arrLen = arrDiv.length; i < arrLen;)arrDiv[i].addEventListener("touchstart", function () {
+    console.log("touchstart")
+}), arrDiv[i].addEventListener("touchmove", function () {
+    console.log("touchmove")
+}), arrDiv[i].addEventListener("touchend", function () {
+    console.log("touchend")
+}), i++, console.log("hello me");
